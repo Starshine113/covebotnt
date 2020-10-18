0dcbdbef5b3fb52b6c6d7e823fcf7fe48c0d6e95
Emily <emily@zukrosi.org>
2020-10-18 03:12:50 +0200

    misc fixes

--- ownerCommands.go
+++ ownerCommands.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
 )
+
+func commandGetUser(args []string, s *discordgo.Session, m *discordgo.MessageCreate) {
+	perms := checkOwner(m.Author.ID)
+	if perms != nil {
+		commandError(perms, s, m)
+		return
+	}
+	if len(args) != 1 {
+		commandError(&errorNotEnoughArgs{2, len(args)}, s, m)
+		return
+	}
+	user, err := parseUser(s, args[0])
+	if err != nil {
+		commandError(err, s, m)
+	}
+	s.ChannelMessageSend(m.ChannelID, user.Username)
+}
 
 func commandSetStatus(args []string, s *discordgo.Session, m *discordgo.MessageCreate) {
 	// this command needs bot owner permissions
@@ -14,14 +32,47 @@ func commandSetStatus(args []string, s *discordgo.Session, m *discordgo.MessageC
 		return
 	}
 
-	// this command needs at least 1 argument
-	if len(args) == 0 {
-		commandError(&errorNotEnoughArgs{1, 0}, s, m)
+	// this command needs at least 2 arguments
+	if len(args) < 2 {
+		commandError(&errorNotEnoughArgs{2, len(args)}, s, m)
 		return
 	}
 
-	// set the status to the specified string
-	status := strings.Join(args, " ")
-	dg.UpdateStatus(0, status)
-	s.ChannelMessageSend(m.ChannelID, "Set status to `+"+status+"`")
+	// check the first arg -- boolean between -replace and -append
+	if args[0] == "-replace" {
+		config.Bot.CustomStatus.Override = true
+	} else if args[0] == "-append" {
+		config.Bot.CustomStatus.Override = false
+	} else {
+		commandError(&errorMissingRequiredArgs{"<-replace/-append> [-clear] <status string>", "<-replace/-append>"}, s, m)
+		return
+	}
+
+	// set custom status to the specified string
+	config.Bot.CustomStatus.Status = strings.Join(args[1:], " ")
+
+	// check second argument -- if it's `-clear` the custom status is cleared
+	if args[1] == "-clear" {
+		config.Bot.CustomStatus.Status = ""
+	}
+
+	// set the status
+	newStatus := config.Bot.Prefixes[0] + "help | in " + fmt.Sprint(len(s.State.Guilds)) + " guilds"
+	if config.Bot.CustomStatus.Status != "" {
+		newStatus += " | " + config.Bot.CustomStatus.Status
+	}
+	if config.Bot.CustomStatus.Override {
+		newStatus = config.Bot.CustomStatus.Status
+	}
+	err := dg.UpdateStatus(0, newStatus)
+	if err != nil {
+		commandError(err, s, m)
+		return
+	}
+	_, err = s.ChannelMessageSend(m.ChannelID, "Set status to `"+newStatus+"`")
+	if err != nil {
+		sugar.Errorf("Error when sending message: ", err)
+		return
+	}
+	sugar.Infof("Updated status to \"%v\"", newStatus)
 }
