2b8821078bdc9e9806949340542a2b487dcb5521
Emily <emily@zukrosi.org>
2020-11-19 19:21:39 +0100

    fix rounding ping times

--- commands/usercommands/ping.go
+++ commands/usercommands/ping.go
@@ -9,7 +9,7 @@ import (
 
 // Ping command: replies with latency and message edit time
 func Ping(ctx *crouter.Ctx) (err error) {
-	heartbeat := ctx.Session.HeartbeatLatency().String()
+	heartbeat := ctx.Session.HeartbeatLatency().Round(time.Millisecond).String()
 
 	// get current time
 	cmdStart := time.Now()
@@ -21,7 +21,7 @@ func Ping(ctx *crouter.Ctx) (err error) {
 	}
 
 	// get time difference, edit message
-	diff := time.Now().Sub(cmdStart).String()
+	diff := time.Now().Sub(cmdStart).Round(time.Millisecond).String()
 	_, err = ctx.EditEmbedf(msg, "Pong!", "Heartbeat: %v\nMessage latency: %v", heartbeat, diff)
 	return err
 }
