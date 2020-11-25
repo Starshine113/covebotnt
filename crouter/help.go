ae70aef7a036f59dcb8e9fa68bc543719163a559
Emily <emily@zukrosi.org>
2020-11-25 17:14:42 +0100

    make Help use ctx.PagedEmbed()

--- crouter/help.go
+++ crouter/help.go
@@ -114,54 +114,8 @@ func (r *Router) Help(ctx *Ctx, guildSettings *structs.GuildSettings) (err error
 			})
 		}
 
-		msg, err := ctx.Send(embeds[0])
-		if err != nil {
-			return err
-		}
-		if err = ctx.Session.MessageReactionAdd(ctx.Channel.ID, msg.ID, "⬅️"); err != nil {
-			return err
-		}
-		if err = ctx.Session.MessageReactionAdd(ctx.Channel.ID, msg.ID, "➡️"); err != nil {
-			return err
-		}
-		if err = ctx.Session.MessageReactionAdd(ctx.Channel.ID, msg.ID, "❌"); err != nil {
-			return err
-		}
-
-		ctx.AdditionalParams["page"] = 0
-		ctx.AdditionalParams["embeds"] = embeds
-
-		ctx.AddReactionHandler(msg.ID, "⬅️", func(ctx *Ctx) {
-			page := ctx.AdditionalParams["page"].(int)
-			embeds := ctx.AdditionalParams["embeds"].([]*discordgo.MessageEmbed)
-
-			if ctx.Message.GuildID != "" {
-				ctx.Session.MessageReactionRemove(ctx.Channel.ID, msg.ID, "⬅️", ctx.Author.ID)
-			}
-
-			if page == 0 {
-				return
-			}
-			ctx.Edit(msg, embeds[page-1])
-			ctx.AdditionalParams["page"] = page - 1
-		})
-
-		ctx.AddReactionHandler(msg.ID, "➡️", func(ctx *Ctx) {
-			page := ctx.AdditionalParams["page"].(int)
-			embeds := ctx.AdditionalParams["embeds"].([]*discordgo.MessageEmbed)
-
-			if ctx.Message.GuildID != "" {
-				ctx.Session.MessageReactionRemove(ctx.Channel.ID, msg.ID, "➡️", ctx.Author.ID)
-			}
-
-			if page >= len(embeds)-1 {
-				return
-			}
-			ctx.Edit(msg, embeds[page+1])
-			ctx.AdditionalParams["page"] = page + 1
-		})
-
-		return nil
+		_, err = ctx.PagedEmbed(embeds)
+		return err
 	}
 
 	var cmd *Command
