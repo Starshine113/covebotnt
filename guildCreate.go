6f9c3f51abfedaf3f2f7903bc48b9dfce91dc5e1
Starshine System <us@starshines.xyz>
2021-02-24 16:59:14 +0100

    also leave guilds owned by blocked users

--- guildCreate.go
+++ guildCreate.go
@@ -11,6 +11,7 @@ func guildJoin(s *discordgo.Session, guild *discordgo.GuildCreate) {
 
 	sugar.Debugf("Joined guild %v (%v)", guild.ID, guild.Name)
 
+	// leave banned guilds
 	for _, id := range config.Bot.BannedServers {
 		if id == guild.ID {
 			err = s.GuildLeave(guild.ID)
@@ -19,6 +20,18 @@ func guildJoin(s *discordgo.Session, guild *discordgo.GuildCreate) {
 				return
 			}
 			sugar.Infof("Automatically left banned guild %v (%v).", guild.Name, guild.ID)
+			return
+		}
+	}
+	// if the owner is a blocked user, leave the guild too
+	for _, id := range config.Bot.BlockedUsers {
+		if id == guild.OwnerID {
+			err = s.GuildLeave(guild.ID)
+			if err != nil {
+				sugar.Errorf("Error leaving guild %v (%v): %v", guild.Name, guild.ID, err)
+				return
+			}
+			sugar.Infof("Automatically left guild %v (%v), owned by blocked user %v.", guild.Name, guild.ID, guild.OwnerID)
 			return
 		}
 	}
